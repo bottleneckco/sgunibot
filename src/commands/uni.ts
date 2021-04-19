2459087dcb384e5b11bca5b8260ce5fa3606af3e
Duncan Leo <hello@duncanleo.me>
2021-04-19 19:12:01 +0800

    chore: update formatting

--- src/commands/uni.ts
+++ src/commands/uni.ts
@@ -37,15 +37,13 @@ const uni: Command = {
           'Asia/Singapore'
         );
 
-        message += `\`${padEnd(currentPeriod.type, 10)}\` ${moment
+        message += `\`${padEnd(currentPeriod.type, 10)}\` ends in ${moment
           .tz(currentPeriodEnd, 'Asia/Singapore')
-          .diff(now, 'days')}d remaining _(till ${currentPeriodEnd.format(
-          'D MMM YYYY'
-        )})_\n`;
+          .diff(now, 'days')}d _(${currentPeriodEnd.format('D MMM')})_\n`;
       }
 
       if (nextPeriod) {
-        message += `\`${padEnd(nextPeriod.type, 10)}\` in ${moment
+        message += `\`${padEnd(nextPeriod.type, 10)}\` begins in ${moment
           .tz(nextPeriod.date_start, 'Asia/Singapore')
           .diff(now, 'days')}d\n`;
       }
@@ -54,7 +52,7 @@ const uni: Command = {
         currentPeriod?.type !== 'vacation' &&
         nextPeriod?.type !== 'vacation'
       ) {
-        message += `\`vacation  \` in ${daysToVacation}d`;
+        message += `\`vacation  \` begins in ${daysToVacation}d`;
       }
 
       message += '\n\n';
