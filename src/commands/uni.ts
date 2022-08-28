be6e3ddd1df583efe7bb73da55bc9fe5e88bd4ba
Duncan Leo <hello@duncanleo.me>
2022-08-28 23:41:45 +0800

    feat: port over /uni command

---
+++ src/commands/uni.ts
@@ -0,0 +1,109 @@
+import { padEnd } from 'lodash';
+import moment from 'moment-timezone';
+
+import Command from '../Command';
+import getStatus from '../util/getStatus';
+
+const PADDING_LENGTH = 12;
+
+function getPeriodMessage(period: SchoolSemesters.Period): string {
+  if (period.week_no != null) {
+    return padEnd(`class wk ${period.week_no}`, PADDING_LENGTH);
+  }
+
+  return padEnd(period.type, PADDING_LENGTH);
+}
+
+export default class UniCommand extends Command {
+  get name(): string {
+    return 'uni';
+  }
+
+  get stages(): App.Stage<this>[] {
+    return [
+      {
+        type: 'text',
+        trigger: {
+          type: 'command',
+        },
+        async handle() {
+          const now = moment.tz('Asia/Singapore');
+          const currentStatus = await getStatus(now);
+
+          let message = '';
+
+          for (const [uni, status] of Object.entries(currentStatus)) {
+            const {
+              term,
+              daysToVacation,
+              nextPeriod,
+              currentPeriod,
+              prevPeriod,
+            } = status;
+
+            message += `<b>${uni}</b>\n`;
+            message += `${term.label}\n`;
+
+            if (currentPeriod) {
+              const currentPeriodEnd = moment.tz(
+                currentPeriod.date_end,
+                'Asia/Singapore'
+              );
+
+              // Fixes diff counting by using inclusive end
+              currentPeriodEnd.set('hour', 23);
+              currentPeriodEnd.set('minute', 59);
+              currentPeriodEnd.set('second', 59);
+
+              message += '<pre>';
+              message += getPeriodMessage(currentPeriod);
+              message += '</pre>';
+              message += ' ';
+              message += '(ongoing, ';
+              message += moment
+                .tz(currentPeriodEnd, 'Asia/Singapore')
+                .from(now, true);
+              message += ' to go)';
+              message += '\n';
+            }
+
+            if (nextPeriod) {
+              message += '<pre>';
+              message += getPeriodMessage(nextPeriod);
+              message += '</pre>';
+              message += ' ';
+              message += '(';
+              message += moment
+                .tz(nextPeriod.date_start, 'Asia/Singapore')
+                .from(now);
+              message += ')';
+              message += '\n';
+            }
+
+            if (
+              currentPeriod?.type !== 'vacation' &&
+              nextPeriod?.type !== 'vacation'
+            ) {
+              message += `<pre>${padEnd(
+                'vacation',
+                PADDING_LENGTH
+              )}</pre> (in ${daysToVacation} days)`;
+            }
+
+            message += '\n\n';
+          }
+
+          return [
+            {
+              type: 'text',
+              text: message,
+              options: {
+                parse_mode: 'HTML',
+              },
+            },
+          ];
+        },
+      },
+    ];
+  }
+}
---
+++ src/util/getStatus.ts
@@ -0,0 +1,125 @@
+import axios from 'axios';
+import { find, findLast, first, flatMap, last } from 'lodash';
+import moment, { Moment } from 'moment-timezone';
+
+const UNIVERSITIES: Record<string, string> = {
+  DigiPen: 'https://fourthclasshonours.github.io/school-semesters/DigiPen.json',
+  NTU: 'https://fourthclasshonours.github.io/school-semesters/NTU.json',
+  NUS: 'https://fourthclasshonours.github.io/school-semesters/NUS.json',
+  SMU: 'https://fourthclasshonours.github.io/school-semesters/SMU.json',
+  SUSS: 'https://fourthclasshonours.github.io/school-semesters/SUSS.json',
+  SUTD: 'https://fourthclasshonours.github.io/school-semesters/SUTD.json',
+};
+
+const getTermBounds = (term: SchoolSemesters.Term) => {
+  return {
+    date_start: first(term.periods)?.date_start,
+    date_end: last(term.periods)?.date_end,
+  };
+};
+
+const getDaysToPeriod = (
+  date: Moment,
+  term: SchoolSemesters.Term,
+  periodType: SchoolSemesters.PeriodType
+) => {
+  const period = find(term.periods, (period) => period.type === periodType);
+
+  if (!period) {
+    throw new Error(`no ${periodType} period!`);
+  }
+
+  return moment.tz(period.date_start, 'Asia/Singapore').diff(date, 'days');
+};
+
+const getDaysToVacation = (date: Moment, term: SchoolSemesters.Term) => {
+  const vacation = findLast(
+    term.periods,
+    (period) => period.type === 'vacation'
+  );
+
+  if (!vacation) {
+    throw new Error('this term has no vacation');
+  }
+
+  return moment.tz(vacation.date_start, 'Asia/Singapore').diff(date, 'days');
+};
+
+type Status = {
+  [uni: string]: {
+    term: SchoolSemesters.Term;
+    daysToVacation: number;
+    prevPeriod: SchoolSemesters.Period | null;
+    currentPeriod: SchoolSemesters.Period | null;
+    nextPeriod: SchoolSemesters.Period | null;
+  };
+};
+
+export default async function getStatus(
+  date = moment.tz('Asia/Singapore')
+): Promise<Status> {
+  const result: Status = {};
+
+  for (const [, url] of Object.entries(UNIVERSITIES)) {
+    const response = await axios.get<SchoolSemesters.Uni>(url);
+
+    let currentTerm: SchoolSemesters.Term | null = null;
+    let prevPeriod: SchoolSemesters.Period | null = null;
+    let nextPeriod: SchoolSemesters.Period | null = null;
+    let currentPeriod: SchoolSemesters.Period | null = null;
+
+    const termsFlattened = flatMap(response.data.terms, (term) => {
+      return term.periods.map((period) => {
+        const termPeriodPair = {
+          term,
+          period,
+        };
+
+        return termPeriodPair;
+      });
+    });
+
+    let prevPeriodIndex = -1;
+    let currentPeriodIndex = -1;
+
+    for (const [index, termPeriodPair] of termsFlattened.entries()) {
+      const { term, period } = termPeriodPair;
+
+      if (date.isAfter(moment.tz(period.date_end, 'Asia/Singapore'), 'days')) {
+        prevPeriod = period;
+        prevPeriodIndex = index;
+        currentTerm = term;
+      } else if (
+        date.isBetween(
+          moment.tz(period.date_start, 'Asia/Singapore'),
+          moment.tz(period.date_end, 'Asia/Singapore'),
+          'days',
+          '[]'
+        )
+      ) {
+        currentPeriod = period;
+        currentPeriodIndex = index;
+        currentTerm = term;
+      } else {
+        break;
+      }
+    }
+
+    const nextPeriodIndex =
+      currentPeriodIndex === -1 ? prevPeriodIndex + 1 : currentPeriodIndex + 1;
+
+    nextPeriod = termsFlattened[nextPeriodIndex]?.period ?? null;
+
+    if (currentTerm != null) {
+      result[response.data.name] = {
+        term: currentTerm,
+        daysToVacation: getDaysToVacation(date, currentTerm),
+        prevPeriod,
+        nextPeriod,
+        currentPeriod,
+      };
+    }
+  }
+
+  return result;
+}
